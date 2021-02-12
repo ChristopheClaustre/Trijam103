87545d26a819c9ef1068078099675211f65a84a2
ChristopheClaustre <christophe.claustre.31@gmail.com>
2021-02-12 21:31:20 +0100

    [FCT] now we are using the post process shader to do our zawarudo effect

--- Assets/Scripts/ZawarudoManager.cs
+++ Assets/Scripts/ZawarudoManager.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 
 public class ZawarudoManager : MonoBehaviour
 {
-    [SerializeField] protected string shaderTimeProperty = "_FreezeTime";
-    [SerializeField] protected string shaderReversedProperty = "_ReversedFreeze";
+    private const string HFovName = "_HFov";
+    private const string VFovName = "_VFov";
+    private const string MaxName = "_DistanceMax";
+    private const string MinName = "_DistanceMin";
+    private const string SafeName = "_DistanceSafe";
 
     public GameEvent launchZawarudoEvent;
     public GameEvent endFreezeEvent;
 
     public GameState gameState;
+
+    public PostProcessShader ppShader;
+    [Range(0, 1.20f)] public float distanceMin;
+    [Range(0, 1.20f)] public float distanceMax;
+    [Range(0, 1.20f)] public float distanceSafe;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +27,17 @@ public class ZawarudoManager : MonoBehaviour
         gameState.Init();
         StartCoroutine(LaunchZawarudo());
 
-        Shader.SetGlobalFloat(shaderTimeProperty, 0);
-        Shader.SetGlobalInt(shaderReversedProperty, 0);
+        Camera cam = ppShader.GetComponent<Camera>();
+        ppShader.Material.SetFloat(HFovName, cam.fieldOfView * Mathf.Deg2Rad);
+        ppShader.Material.SetFloat(VFovName, cam.fieldOfView * cam.aspect * Mathf.Deg2Rad);
+        ppShader.Material.SetFloat(SafeName, distanceSafe);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ppShader.Material.SetFloat(MinName, distanceMin);
+        ppShader.Material.SetFloat(MaxName, distanceMax);
     }
 
     IEnumerator LaunchZawarudo()
@@ -34,8 +45,6 @@ public class ZawarudoManager : MonoBehaviour
         yield return new WaitUntil(() => Input.GetKey(KeyCode.Space));
 
         launchZawarudoEvent.Raise();
-        Shader.SetGlobalFloat(shaderTimeProperty, Time.timeSinceLevelLoad);
-        Shader.SetGlobalInt(shaderReversedProperty, 0);
     }
 
     public void LaunchFreezeTimer()
@@ -48,13 +57,11 @@ public class ZawarudoManager : MonoBehaviour
         yield return new WaitForSeconds(gameState.FreezeDuration);
 
         endFreezeEvent.Raise();
-        Shader.SetGlobalFloat(shaderTimeProperty, Time.timeSinceLevelLoad);
-        Shader.SetGlobalInt(shaderReversedProperty, 1);
     }
 
     private void OnDestroy()
     {
-        Shader.SetGlobalFloat(shaderTimeProperty, 0);
-        Shader.SetGlobalInt(shaderReversedProperty, 0);
+        ppShader.Material.SetFloat(MinName, 0);
+        ppShader.Material.SetFloat(MaxName, 0);
     }
 }
